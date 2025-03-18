5436163c064008643a750f8fafe02779b7cc2221
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-03-18 21:37:28 +0800

    hostility compat

---
+++ src/main/java/dev/xkmc/glimmeringtales/init/data/GTHostilityGen.java
@@ -0,0 +1,65 @@
+package dev.xkmc.glimmeringtales.init.data;
+
+import dev.xkmc.glimmeringtales.content.item.wand.WandHandleItem;
+import dev.xkmc.glimmeringtales.init.GlimmeringTales;
+import dev.xkmc.glimmeringtales.init.reg.GTItems;
+import dev.xkmc.l2backpack.content.common.BaseBagItem;
+import dev.xkmc.l2core.init.reg.varitem.VarHolder;
+import dev.xkmc.l2core.serial.config.ConfigDataProvider;
+import dev.xkmc.l2hostility.content.config.WeaponConfig;
+import dev.xkmc.l2hostility.init.L2Hostility;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.HolderSet;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class GTHostilityGen extends ConfigDataProvider {
+
+	public GTHostilityGen(DataGenerator generator, CompletableFuture<HolderLookup.Provider> pvd) {
+		super(generator, pvd, "Glimmering Tales x L2Hostility");
+	}
+
+	@Override
+	public void add(Collector collector) {
+		var config = new WeaponConfig();
+		var empty = new WeaponConfig.ItemConfig(new ArrayList<>(List.of(ItemStack.EMPTY)), 0, 1000);
+		var lv1 = new WeaponConfig.ItemConfig(new ArrayList<>(List.of(
+				of(GTItems.GOLD_WAND, GTItems.EARTHQUAKE),
+				of(GTItems.ENDER_WAND, GTItems.WINTER_STORM),
+				of(GTItems.OCEAN_WAND, GTItems.OCEAN_SHELTER)
+		)), 150, 100);
+		var lv2 = new WeaponConfig.ItemConfig(new ArrayList<>(List.of(
+				of(GTItems.GOLD_WAND, GTItems.AMETHYST_PENETRATION),
+				of(GTItems.NETHER_WAND, GTItems.HELL_MARK),
+				of(GTItems.ENDER_WAND, GTItems.SNOW_TORNADO)
+		)), 250, 50);
+		var lv3 = new WeaponConfig.ItemConfig(new ArrayList<>(List.of(
+				of(GTItems.THUNDER_WAND, GTItems.THUNDERSTORM),
+				of(GTItems.THUNDER_WAND, GTItems.CHARGE_BURST),
+				of(GTItems.THUNDER_WAND, GTItems.RUNE_THUNDER)
+		)), 350, 50);
+		config.special_weapons.put(HolderSet.direct(
+				EntityType.ZOMBIE.builtInRegistryHolder(),
+				EntityType.HUSK.builtInRegistryHolder(),
+				EntityType.SKELETON.builtInRegistryHolder(),
+				EntityType.ZOMBIFIED_PIGLIN.builtInRegistryHolder(),
+				EntityType.STRAY.builtInRegistryHolder(),
+				EntityType.BOGGED.builtInRegistryHolder(),
+				EntityType.WITHER_SKELETON.builtInRegistryHolder()
+		), new ArrayList<>(List.of(empty, lv1, lv2, lv3)));
+		collector.add(L2Hostility.WEAPON, GlimmeringTales.loc("spells"), config);
+	}
+
+	public static ItemStack of(VarHolder<WandHandleItem> wand, VarHolder<?> spell) {
+		ItemStack ans = GTItems.WAND.asStack();
+		GTItems.WAND_HANDLE.set(ans, wand.item());
+		BaseBagItem.setItems(ans, List.of(spell.item().asStack()));
+		return ans;
+	}
+
+}
